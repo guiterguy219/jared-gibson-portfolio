c36c7698ce0736bb029026c058360eefcbd6f5cf
Jared Gibson <jared.gibson@live.com>
2025-12-12 15:16:42 -0700

    Update time-since comp to use time block

--- src/components/custom-ui/time-since.tsx
+++ src/components/custom-ui/time-since.tsx
@@ -1,11 +1,11 @@
-import { formatDistanceToNow } from "date-fns";
+import { format, formatDistanceToNow } from "date-fns";
 
 export default function TimeSince({ date }: { date: string }) {
   return (
-    <>
+    <time dateTime={date} title={format(date, "PPpp")}>
       {formatDistanceToNow(date, {
         addSuffix: true,
       })}
-    </>
+    </time>
   );
 }
